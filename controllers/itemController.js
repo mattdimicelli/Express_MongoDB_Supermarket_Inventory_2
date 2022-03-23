c22534a16db0619875b7f156be0c3985b133b54c
Matt Di Micelli <matthewdimicelli@gmail.com>
2022-03-23 14:19:55 -0400

    complete item view

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,5 +1,6 @@
 const Item = require('../models/Item.js');
 const Department = require('../models/Department.js');
+const mongoose = require('mongoose');
 
 // Display home page for inventory
 exports.homeGet = async (req, res) => {
@@ -11,13 +12,15 @@ exports.homeGet = async (req, res) => {
 exports.itemsGet = async(req, res) => {
     const items = await Item.find({}).sort({'name': 'asc'}).populate('department').exec();
     const depts = await Department.find({}, 'name url').sort({ 'name': 'asc' }).exec();
-    console.log(items);
     res.render('items', {depts, items, title: 'All Items'});
 }
 
 // Display details for an individual item
-exports.itemGet = (req, res) => {
-    res.send('NOT YET IMPLEMENTED');
+exports.itemGet = async(req, res) => {
+    const id = mongoose.Types.ObjectId(req.params.id);
+    const item = await Item.findById(id).populate('department').exec();
+    const depts = await Department.find({}, 'name url').sort({ 'name': 'asc' }).exec();
+    res.render('item', {depts, item, title: item.name });
 }
 
 // Handle item update form on GET
